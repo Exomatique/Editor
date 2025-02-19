cd1fd3f745bab3a7150e07a573b1f15a71c5634b
Wyrdix <wyrdix@gmail.com>
2025-02-19 17:39:58 +0100

    Initial lib commit

---
+++ src/lib/Editor.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+</script>
+
+<div
+	class="text-body-color-dark bg-surface-50 rounded-3x relative min-h-20 w-full py-20 shadow-2xl"
+></div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Editor from '$lib/Editor.svelte';
+</script>
+
+<div class="relative flex w-full flex-1 flex-row justify-center">
+	<div class="absolute h-full w-3/4 flex-1 flex-row">
+		<Editor />
+	</div>
+</div>
