5f612038ebd0fae177afadd45794507838b7bdbd
Wyrdix <wyrdix@gmail.com>
2025-02-20 17:24:49 +0100

    Add text tooltip

--- src/lib/modules/text/TextBlock.svelte
+++ src/lib/modules/text/TextBlock.svelte
@@ -4,4 +4,11 @@
 	let { data = $bindable() }: { data: TextData } = $props();
 </script>
 
-<div class="w-full flex-1 border-none outline-none" contenteditable bind:innerText={data}></div>
+<div class="relative">
+	<div class="w-full flex-1 border-none outline-none" contenteditable bind:innerText={data}></div>
+	{#key data}
+		{#if data.length === 0 || data === '\n'}
+			<div class="pointer-events-none absolute left-0 top-0 text-surface-400">Type text</div>
+		{/if}
+	{/key}
+</div>
