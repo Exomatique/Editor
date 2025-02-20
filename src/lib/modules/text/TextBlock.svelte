19abaacee710a833f0d0b23cc66e79add4704fd7
Wyrdix <wyrdix@gmail.com>
2025-02-20 17:48:47 +0100

    Have test module be rendered as markdown text

--- src/lib/modules/text/TextBlock.svelte
+++ src/lib/modules/text/TextBlock.svelte
@@ -1,13 +1,45 @@
 <script lang="ts">
 	import type { TextData } from './TextData.js';
+	import remarkParse from 'remark-parse';
+	import remarkRehype from 'remark-rehype';
+	import rehypeSanitize from 'rehype-sanitize';
+	import rehypeStringify from 'rehype-stringify';
+	import { unified } from 'unified';
 
 	let { data = $bindable() }: { data: TextData } = $props();
+
+	let html_data = $state('');
+
+	const html_data_updater = async (data: string) => {
+		return await unified()
+			.use(remarkParse)
+			.use(remarkRehype)
+			.use(rehypeSanitize)
+			.use(rehypeStringify)
+			.process(data)
+			.then(String);
+	};
+
+	let focused = $state(false);
+
+	$effect(() => {
+		html_data_updater(data).then((v) => {
+			html_data = v;
+			console.log(html_data);
+		});
+	});
 </script>
 
-<div class="relative">
-	<div class="w-full flex-1 border-none outline-none" contenteditable bind:innerText={data}></div>
+<div class="relative" onfocusin={() => (focused = true)} onfocusout={() => (focused = false)}>
+	{#if focused}
+		<div class="w-full flex-1 outline-none" contenteditable bind:innerText={data}></div>
+	{:else}
+		<div class="min-h-5 w-full flex-1 border-none outline-none" contenteditable>
+			{@html html_data}
+		</div>
+	{/if}
 	{#key data}
-		{#if data.length === 0 || data === '\n'}
+		{#if !focused && (data.length === 0 || data === '\n')}
 			<div class="pointer-events-none absolute left-0 top-0 text-surface-400">Type text</div>
 		{/if}
 	{/key}
